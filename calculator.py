a2d9fb9f5a616ed547289cee05a3d1f1a6f50adf
shekar bannennoolla <shekharbannennoolla@gmail.com>
2026-02-26 10:22:26 +0530

    added line266

--- calculator.py
+++ calculator.py
@@ -13,14 +13,17 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 if choice == '1':
-    print("Result:", num1 + num2)
-
+    print("Result:", num1 + num2) 
+elif choice == '2':
+    print("Result:", num1 - num2)       
+elif choice == '3':
+    print("Result:", num1 * num2)           
 
 elif choice == '4':
     if num2 != 0:
         print("Result:", num1 / num2)
     else:
-        print("Error! Division by zero is not allowed.")
+        print("Error! Division by zero is not allowed.5555555555555566666")
 
 else:
     print("Invalid Input")
